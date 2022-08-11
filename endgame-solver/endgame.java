72255aec2c211ffa19f58f542e13eec02062ddcb
ali161102 <ali16miller@hotmail.co.uk>
2022-08-12 00:11:24 +0100

    initial building of valid board indices

---
+++ endgame-solver/endgame.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class endgame {
+    
+    public static ArrayList<Integer> valid_indices;
+    public static int[] board;
+    public static int[] marbles;
+
+    public endgame() {
+        board = new int[284];
+        marbles = new int[10];
+    }
+    
+    public static void setValidSpaces() {
+        /* builds the array of indices that are actually board positions */
+        valid_indices = new ArrayList<Integer>();
+
+        int[] middle_starts = {4, 21, 38, 55, 68, 86, 104, 122, 140, 157, 174, 191, 208, 230, 248, 266, 284};
+        int[] middle_ends   = {4, 22, 40, 58, 80, 97, 114, 131, 148, 166, 184, 202, 220, 233, 250, 267, 284};
+
+        for (int i = 0; i < middle_starts.length; i++) {
+            IntStream.range(middle_starts[i], middle_ends[i]+1).forEach(n -> valid_indices.add(n));
+        }      
+    }
+
+    public static void main(String[] args) {
+        setValidSpaces();
+        System.out.println(valid_indices);       
+        System.out.println(valid_indices.size()); 
+    }
+
+}
