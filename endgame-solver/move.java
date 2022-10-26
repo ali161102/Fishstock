1f685fcc8c3dd127247ca11f45473663a990e0eb
ali161102 <ali16miller@hotmail.co.uk>
2022-10-26 16:11:52 +0100

    add move_generator finding the paths to each reachable space

--- endgame-solver/move.java
+++ endgame-solver/move.java
@@ -1,7 +1,37 @@
 import java.util.ArrayList;
 
 public class move {
+
+    public move(int start, int end, ArrayList<Integer> mid){
+        this.startSpace = start;
+        this.endSpace = end;
+        this.midSpaces = mid;
+    }
+    
     public int startSpace;
     public ArrayList<Integer> midSpaces; // each move can consist of many consequent jumps
     public int endSpace;
+
+    public static ArrayList<Integer> getPath(move m) {
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        path.add(m.startSpace);
+        path.addAll(m.midSpaces);
+        path.add(m.endSpace);
+
+        return path;
+    }
+
+    public void printMove() {
+        String output = Integer.toString(startSpace) + " -> ";
+
+        if (midSpaces.size() > 0) {
+            for (int space : midSpaces) {
+                output = output + Integer.toString(space) + " -> ";
+            }
+        }
+
+        output += Integer.toString(endSpace);
+
+        System.out.println(output);
+    }
 }
--- endgame-solver/move_generator.java
+++ endgame-solver/move_generator.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,63 +8,44 @@ class move_generator {
 
     static int[][] numSpacesToEdge;
     static int[] directionOffsets = precomputed.directionOffsets;
+    //public static board b;
 
-    public static ArrayList<Integer> generateMoves(board b) {
+    public static ArrayList<move> generateMoves(board b) {
 
         precomputed precalc = new precomputed();
 
         numSpacesToEdge = precomputed.numSpacesToEdge;
-        //int[] directionOffsets = precomputed.directionOffsets;
-
-
-        //System.out.println(Arrays.toString(precomputed.numSpacesToEdge[67]));
-
+  
         ArrayList<move> moves = new ArrayList<move>();
-
-        ArrayList<int[]> endpoints = new ArrayList<int[]>();
-
         ArrayList<Integer> reachable = new ArrayList<Integer>();
+        HashMap<Integer, Integer> predecessors = new HashMap<Integer, Integer>();
 
         Queue<Integer> queue = new LinkedList<Integer>();
 
-        
         // for each marble's space index:
         for (int index : b.marbles) {
 
+            // reset these for each marble
+            reachable.clear();
+            predecessors.clear(); 
+
+            /* do a bfs from position to find all possible ending spaces */
             queue.add(index);
-            System.out.println("queue = " + queue);
-            int original = index;
-
             while(!queue.isEmpty()) {
-
                 int current = queue.remove();
-                System.out.println("");
-                System.out.println("current set to " + current);
 
-                System.out.println("current's spacestoedge " + Arrays.toString(numSpacesToEdge[current]));
 
-                /* do a bfs from position to find all possible ending spaces */
-                for (int dir = 0; dir < 6; dir++) {
-                // in each direction, 
-                        
+                for (int dir = 0; dir < 6; dir++) { // in each direction, 
+                
                         int closest = -1;
 
-                        System.out.println("direction = " + dir);
+                        for (int dist = 1; dist < ((numSpacesToEdge[current][dir])/2)+1; dist++) { // look for the next closest marble that could be a valid jump
+                        
+                            int check = current + (dist * directionOffsets[dir]); // get index of space to check by adding offset
 
-                        for (int dist = 1; dist < ((numSpacesToEdge[current][dir])/2)+1; dist++) {
-                        // from 1 space away to <distToEdge//2>, look for the next closest marble
-
-                            int check = current + (dist * directionOffsets[dir]);
-                            System.out.println("dist = " + dist + ", so check = " + check);
-                            
-                            if (b.space[check] > 0) {
-                                // there is a marble in the space
-                                closest = dist;
-                                System.out.println("the closest marble is in space " + check);
-
+                            if ((b.space[check] > 0) && (check != index)) {
+                                closest = dist; // marble found in this direction
                                 break;
-
-
                             }
                         }
                         
@@ -73,17 +55,13 @@ class move_generator {
                         // if there is one:
                         if (closest != -1) {
                             
-                            System.out.println("check all spaces to landing space");
                             // check if all spaces from there to the potental landing space are free
-
                             for (int dist = closest+1; dist < closest*2+1; dist++) {
 
                                 int check = current + (dist * directionOffsets[dir]);
                                 landing = check;
-                                System.out.println("checking jump " + current + " -- " + (current + closest*directionOffsets[dir]) + " --> " + landing);
 
                                 if (b.space[check] > 0) {
-                                    System.out.println("there is a marble in " + check + ", so cannot do the jump");
                                     // there is a marble in the space, so cannot do the jump
                                     canjump = false;
                                     break;
@@ -91,49 +69,56 @@ class move_generator {
                             }
                         }
 
-                        if (canjump && landing != -1) {
-                            System.out.println("!! adding the jump " + current + " ----> " + landing);
+                        if (canjump && landing != -1) { // all spaces are clear so jump is possible
 
-                            if (!reachable.contains(landing)) {
-                                // prevents 'loop' moves
+                            if (!reachable.contains(landing)) { //prevents 'loop' moves
+                                
+                                queue.add(landing); // jumps can continue onwards so add to queue to be processed
+                                reachable.add(landing);
+                  
+                                predecessors.put(landing, current); // current -> landing, used to work out the move paths
 
                                 queue.add(landing);
-                                reachable.add(landing);
-                                endpoints.add(new int[]{original, current, landing});
-                                System.out.println("added " + landing + " from " + current + " possibly from " + original);
-
                             }
-                            //queue.add(landing);
-                            
-                            
                             
                         }
                             
-                            // if all spaces are clear:
-                                // add landing space to queue to be processed
                 }
             }
 
-          // return all such spaces found
-        }
-          System.out.println(reachable);
+            /* determine the path to each reachable space */
+            for (int start : reachable) {
+                ArrayList<Integer> path = new ArrayList<Integer>();
 
-          for (int[] move : endpoints) {
-            System.out.println(Arrays.toString(move));
-          }
-          //System.out.println(endpoints);
-          return reachable;
+                int endpointer = predecessors.get(start);
+                // work backwards from the end space by getting the predecessor, until at the starting space
+                while (endpointer != index) { 
+                    path.add(endpointer);
+                    endpointer = predecessors.get(endpointer);
+                }
+                Collections.reverse(path); // as the path was backwards
+
+                move thismove = new move(endpointer, start, path);
+                moves.add(thismove);
+
+            }
+        }
+          return moves;
     }
 
 
     public static void main(String[] args) {
-
-        int[] startlocations = {4, 21, 22, 38, 39, 40, 72, 56, 57, 58}; 
+        int[] startlocations = {4, 38, 40, 56, 57, 58, 74, 75, 91, 105, 126, 129, 177, 180, 198, 214, 217, 230, 233, 266};
         board b = new board(startlocations.length);
         b.initialise(startlocations);
 
-        generateMoves(b);
+        System.out.println("---");
+        //visit(38);
+        ArrayList<move> possiblemoves = generateMoves(b);
 
-        
+        for (move mv : possiblemoves) {
+            //System.out.println(move.getPath(mv));
+            mv.printMove();
+        }
     }
 }
